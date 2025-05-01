c20dedeac34040ece2d1d8dfa0b9dd50fd1c6e85
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2025-05-01 18:55:15 +0200

    feat: use of websocket

--- src/controllers/message.rs
+++ src/controllers/message.rs
@@ -1,5 +1,7 @@
 use axum::{
     Json,
+    extract::ws::{WebSocket, WebSocketUpgrade},
+    response::Response,
     routing::{MethodRouter, post},
 };
 
@@ -9,4 +11,29 @@ pub fn handle_post_message() -> MethodRouter {
         println!("{:?}", event["helloo"].to_string());
         "Event received"
     })
+}
+
+pub async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            println!("WS : Received message: {:?}", msg);
+            msg
+        } else {
+            // client disconnected
+            println!("WS : Client disconnected");
+            return;
+        };
+
+        if socket.send(msg.clone()).await.is_ok() {
+            println!("WS : Sent message: {:?}", msg);
+        } else {
+            // client disconnected
+            println!("WS : Client disconnected");
+            return;
+        }
+    }
+}
+
+pub async fn handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
 }
--- src/routes/main_routes.rs
+++ src/routes/main_routes.rs
@@ -1,7 +1,9 @@
-use crate::controllers::message::handle_post_message;
+use crate::controllers::message::{handle_post_message, handler};
 use axum::Router;
-use axum::routing::post;
+use axum::routing::{any, post};
 
 pub fn get_routes() -> Router {
-    Router::new().route("/debug", post(handle_post_message()))
+    Router::new()
+        .route("/debug", post(handle_post_message()))
+        .route("/ws", any(handler))
 }
